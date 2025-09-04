1f1c4eabc6ea4a9c59ecffad21a311c75ec1b12c
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-09-03 22:43:24 -0300

    refactor: alter docs endpoint

--- routes/web/scalar.php
+++ routes/web/scalar.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Web\ScalarController;
 
-Route::prefix('docs/scalar')->group(function () {
+Route::prefix('docs/api')->group(function () {
     Route::get('/', [ScalarController::class, 'index'])->name('docs.scalar.index');
     Route::get('/spec', [ScalarController::class, 'spec'])->name('docs.scalar.spec');
 });
