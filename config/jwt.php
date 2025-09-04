20faed26f470b2386e10cd35b55bb2298b7ea551
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-09-03 22:42:19 -0300

    feat: add blacklist database support

--- config/jwt.php
+++ config/jwt.php
@@ -10,7 +10,6 @@
  */
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | JWT Authentication Secret
@@ -45,7 +44,6 @@ return [
     */
 
     'keys' => [
-
         /*
         |--------------------------------------------------------------------------
         | Public Key
@@ -82,7 +80,6 @@ return [
         */
 
         'passphrase' => env('JWT_PASSPHRASE'),
-
     ],
 
     /*
@@ -262,7 +259,6 @@ return [
     */
 
     'providers' => [
-
         /*
         |--------------------------------------------------------------------------
         | JWT Provider
@@ -294,8 +290,7 @@ return [
         |
         */
 
-        'storage' => Tymon\JWTAuth\Providers\Storage\Illuminate::class,
-
+        // 'storage' => Tymon\JWTAuth\Providers\Storage\Illuminate::class,
+        'storage' => Kamoca\JwtDatabaseBlacklist\Providers\Storage\Illuminate::class,
     ],
-
 ];
