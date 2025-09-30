8ded4c21e497bbb7b3dd71b5947fcd619eeb12d4
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-30 15:09:54 +0100

    feat: add location routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Location\CityController;
+use App\Http\Controllers\Location\CountryController;
+use App\Http\Controllers\Location\StateController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +25,9 @@ Route::prefix('v1')->group(function () {
         Route::post('user/{user}/roles/assign', [UserController::class, 'assignRoles'])->name('user.roles.assign');
         Route::post('user/{user}/roles/revoke', [UserController::class, 'revokeRoles'])->name('user.roles.revoke');
         Route::post('user/{user}/roles/sync', [UserController::class, 'syncRoles'])->name('user.roles.sync');
+
+        Route::apiResource('country', CountryController::class)->names('country');
+        Route::apiResource('state', StateController::class)->names('state');
+        Route::apiResource('city', CityController::class)->names('city');
     });
 });
