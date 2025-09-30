a852dfb2481e79a9c2d52c7cd898b794369fa0da
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-09-30 15:09:30 +0100

    feat: add location seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
-            // CountrySeeder::class,
-            // StateSeeder::class,
-            // CitySeeder::class,
+            CountrySeeder::class,
+            StateSeeder::class,
+            CitySeeder::class,
         ]);
     }
 }
