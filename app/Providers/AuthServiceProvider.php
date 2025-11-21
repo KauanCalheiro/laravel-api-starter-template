e76a33362970d6a4301988f992d56868a8983d6a
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-11-21 23:38:16 +0000

    refactor: removing city

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,23 +2,14 @@
 
 namespace App\Providers;
 
-use App\Models\Location\City;
-use App\Models\Location\Country;
-use App\Models\Location\State;
 use App\Models\User;
-use App\Policies\CityPolicy;
-use App\Policies\CountryPolicy;
-use App\Policies\StatePolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
-        User::class    => UserPolicy::class,
-        Country::class => CountryPolicy::class,
-        State::class   => StatePolicy::class,
-        City::class    => CityPolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     public function boot()
