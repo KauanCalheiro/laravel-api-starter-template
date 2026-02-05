04aaf7081dde1d8c1acce4ccc7aae94295f9e706
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-02-05 11:22:52 -0300

    refactor: remove unused permissions

--- app/Enums/PermissionEnum.php
+++ app/Enums/PermissionEnum.php
@@ -12,19 +12,4 @@ enum PermissionEnum: string
     case ASSIGN_USER_ROLE = 'user.assign.role';
     case REVOKE_USER_ROLE = 'user.revoke.role';
     case SYNC_USER_ROLE   = 'user.sync.role';
-
-    case CREATE_COUNTRY = 'country.create';
-    case READ_COUNTRY   = 'country.read';
-    case UPDATE_COUNTRY = 'country.update';
-    case DELETE_COUNTRY = 'country.delete';
-
-    case CREATE_STATE = 'state.create';
-    case READ_STATE   = 'state.read';
-    case UPDATE_STATE = 'state.update';
-    case DELETE_STATE = 'state.delete';
-
-    case CREATE_CITY = 'city.create';
-    case READ_CITY   = 'city.read';
-    case UPDATE_CITY = 'city.update';
-    case DELETE_CITY = 'city.delete';
 }
